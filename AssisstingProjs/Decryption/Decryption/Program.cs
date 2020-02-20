b5d1d22edb914f57a2c82bbc15b906212991c421
unknown <hugzg1@hotmail.com>
2020-02-20 13:15:54 -0800

    sync local

---
+++ AssisstingProjs/Decryption/Decryption/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QC.BaseLib.Security;
+
+namespace Decryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ent = CryptoAPI.Encrypt("password", "P@ssW03d");
+            ent = "zEYqsw3XoQHGLgHd+5lqNUhv2vLK1VS38nRKqBT1PVpgQAdM9PyPPPnOm4Ri8G6yWLDn6kB9F9UOX6Gc2Vxk8lNAfFRTVHJJTjY=";
+            string pswdHash = CryptoAPI.ComputeHash("12345678");
+            //string pswd = CryptoAPI.Decrypt("ly7Xesz7x8eNA1DEVKeAHs0XB896Eoz941S6hNjU+Ajh33br8Y6avIcUrlA6ZqW1fKQo2k+bKyniidzVksxJv1NAfFRTVHJJTjY=", "P@ssW03d");
+        }
+    }
+}
---
+++ AssisstingProjs/ResourceBuilder-Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ResourceBuilder_Test.Resources;
+
+namespace ResourceBuilder_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourceManager rm = new ResourceManager("ResourceBuilder_Test.Resources.Explainations", Assembly.GetAssembly(typeof(Program)));
+            //ResourceManager rm = new ResourceManager(typeof(Program));
+            string message = rm.GetString("String1");
+
+            CultureInfo frCulture = new CultureInfo("fr-FR");
+            Thread.CurrentThread.CurrentUICulture = frCulture;
+            //rm = new ResourceManager(typeof(Program));
+            message = rm.GetString("String1");
+            message = Explainations.String1;
+        }
+    }
+}
